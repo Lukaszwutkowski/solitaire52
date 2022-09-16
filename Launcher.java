de83e5bde58a01e82f0984523c9061e7d1e93844
Lukaszwutkowski <64429535+Lukaszwutkowski@users.noreply.github.com>
2022-09-16 19:22:13 +0200

    Playable game, added four piles

--- Launcher.java
+++ Launcher.java
@@ -6,6 +6,6 @@ public class Launcher
     public static void main (String[] args)
     {
         Game myGame = new CustomGame(); 
-        LwjglApplication launcher = new LwjglApplication( myGame, "Game Title", 800, 600 );
+        LwjglApplication launcher = new LwjglApplication( myGame, "Solitaire 52", 800, 600 );
     }
 }
--- LevelScreen.java
+++ LevelScreen.java
@@ -1,7 +1,10 @@
 import com.badlogic.gdx.math.MathUtils;
 
+import java.util.ArrayList;
+
 public class LevelScreen extends BaseScreen
 {
+    private ArrayList<Pile> pileList;
     public void initialize() 
     {        
         BaseActor background = new BaseActor(0, 0, mainStage);
@@ -14,6 +17,28 @@ public class LevelScreen extends BaseScreen
                 int y = MathUtils.random(0, 300);
                 Card c = new Card(x, y, mainStage);
                 c.setRankSuitValues(r, s);
+                c.toBack();
+            }
+        }
+
+        background.toBack();
+
+        pileList = new ArrayList<Pile>();
+        for (int i = 0; i < 4; i++){
+            int pileX = 120 + 150 * i;
+            int pileY = 450;
+            Pile pile = new Pile(pileX, pileY, mainStage);
+            pileList.add(pile);
+        }
+
+        for (BaseActor actor : BaseActor.getList(mainStage, "Card")){
+            Card card = (Card)actor;
+            if (card.getRankValue() == 0){
+                Pile pile = pileList.get(card.getSuitValue());
+                card.toFront();
+                card.moveToActor(pile);
+                pile.addCard(card);
+                card.setDraggable(false);
             }
         }
     }
