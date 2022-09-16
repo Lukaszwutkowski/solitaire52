f7ce0189e295de6000dfecb2964935d25b19b665
Lukaszwutkowski <64429535+Lukaszwutkowski@users.noreply.github.com>
2022-09-16 16:08:15 +0200

    Initialize method in LevelScreen.class updated

--- LevelScreen.java
+++ LevelScreen.java
@@ -1,8 +1,21 @@
+import com.badlogic.gdx.math.MathUtils;
+
 public class LevelScreen extends BaseScreen
 {
     public void initialize() 
     {        
-        
+        BaseActor background = new BaseActor(0, 0, mainStage);
+        background.loadTexture("assets/felt.jpg");
+        BaseActor.setWorldBounds(background);
+
+        for (int r = 0; r < Card.rankNames.length; r++){
+            for (int s = 0; s < Card.suitNames.length; s++){
+                int x = MathUtils.random(0, 800);
+                int y = MathUtils.random(0, 300);
+                Card c = new Card(x, y, mainStage);
+                c.setRankSuitValues(r, s);
+            }
+        }
     }
 
     public void update(float dt)
